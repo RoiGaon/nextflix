ce96807794275c07ded3f5b7c136a03de2ae6b68
RoiGaon <roigaon@gmail.com>
2022-06-04 19:15:43 +0300

    adding banner's styles

--- components/banner/Banner.tsx
+++ components/banner/Banner.tsx
@@ -1,5 +1,9 @@
-import { url } from "inspector";
 import * as React from "react";
+// Next
+import Image from "next/image";
+
+// Styles
+import S from "./Banner.module.css";
 
 interface Props {
   title: string;
@@ -12,19 +16,36 @@ const Banner: React.FC<Props> = ({ title, subTitle, imgUrl }) => {
 
   return (
     <>
-      <h3>{title}</h3>
-      <h3>{subTitle}</h3>
-      <button onClick={handleOnPlay}>Play</button>
-      <div
-        style={{
-          backgroundImage: `url(${imgUrl})`,
-          width: "100%",
-          height: "100%",
-          position: "absolute",
-          backgroundSize: "cover",
-          backgroundPosition: "50% 50%",
-        }}
-      />
+      <div className={S.container}>
+        <div className={S.leftWrapper}>
+          <div className={S.left}>
+            <div className={S.nseriesWrapper}>
+              <p className={S.firstLetter}>N</p>
+              <p className={S.series}>S E R I E S</p>
+            </div>
+            <h3 className={S.title}>{title}</h3>
+            <h3 className={S.subTitle}>{subTitle}</h3>
+
+            <div className={S.playBtnWrapper}>
+              <button className={S.btnWithIcon} onClick={handleOnPlay}>
+                <Image
+                  src="/static/play-arrow.svg"
+                  alt="Play icon"
+                  width={32}
+                  height={32}
+                />
+                <span className={S.playText}>Play</span>
+              </button>
+            </div>
+          </div>
+        </div>
+        <div
+          className={S.bannerImg}
+          style={{
+            backgroundImage: `url(${imgUrl}`,
+          }}
+        ></div>
+      </div>
     </>
   );
 };
