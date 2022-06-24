9ad29f68c387740535e107c809ef63bb8c157445
RoiGaon <roigaon@gmail.com>
2022-06-24 15:54:10 +0300

    adding index to check scale

--- components/card/Card.tsx
+++ components/card/Card.tsx
@@ -11,6 +11,7 @@ type Size = "small" | "medium" | "large";
 interface Props {
   imgUrl: string;
   size: Size;
+  index: number;
 }
 
 const classMap: any = {
@@ -22,15 +23,18 @@ const classMap: any = {
 const defaultImage =
   "https://images.unsplash.com/photo-1485846234645-a62644f84728?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1340&q=80";
 
-const Card: React.FC<Props> = ({ imgUrl = defaultImage, size = "medium" }) => {
+const Card: React.FC<Props> = ({
+  imgUrl = defaultImage,
+  size = "medium",
+  index,
+}) => {
   const [imgSrc, setImgSrc] = React.useState(imgUrl);
   return (
     <>
       <div className={S.container}>
-        Card
         <motion.div
           className={cn(classMap[size], S.imgMotionWrapper)}
-          whileHover={{ scale: 1.2 }}
+          whileHover={index === 0 ? { scaleY: 1.1 } : { scale: 1.1 }}
         >
           <Image
             src={imgSrc}
