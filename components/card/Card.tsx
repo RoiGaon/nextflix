71a58a70b1622f1504e5a939febfa9facab0ed6f
RoiGaon <roigaon@gmail.com>
2022-06-24 14:45:54 +0300

    adding card logic

--- components/card/Card.tsx
+++ components/card/Card.tsx
@@ -1,9 +1,40 @@
 import React from "react";
+// Next
+import Image from "next/image";
+// Styles
+import S from "./Card.module.css";
 
-const Card: React.FC = () => {
+type Size = "small" | "medium" | "large";
+interface Props {
+  imgUrl: string;
+  size: Size;
+}
+
+const classMap: any = {
+  large: S.lgItem,
+  medium: S.mdItem,
+  small: S.smItem,
+};
+
+const defaultImage =
+  "https://images.unsplash.com/photo-1485846234645-a62644f84728?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1340&q=80";
+
+const Card: React.FC<Props> = ({ imgUrl = defaultImage, size = "medium" }) => {
+  const [imgSrc, setImgSrc] = React.useState(imgUrl);
   return (
     <>
-      <div>Card</div>
+      <div className={S.container}>
+        Card
+        <div className={classMap[size]}>
+          <Image
+            src={imgSrc}
+            alt="image"
+            layout="fill"
+            className={S.cardImg}
+            onError={() => setImgSrc(defaultImage)}
+          />
+        </div>
+      </div>
     </>
   );
 };
