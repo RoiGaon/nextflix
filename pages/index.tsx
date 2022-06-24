036bac782d7b9745cb343b1edec8d2d6bb845172
RoiGaon <roigaon@gmail.com>
2022-06-24 18:44:01 +0300

    adding padding

--- pages/index.tsx
+++ pages/index.tsx
@@ -26,21 +26,23 @@ const Home: NextPage = ({
         <meta name="description" content="netflix app" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <NavBar userName="roigaon@gmail.com" />
-      <Banner
-        title="Clifford the red dog"
-        subTitle="a very cute dog"
-        imgUrl="/static/clifford.webp"
-      />
-      <div className={S.sectionWrapper}>
-        <SectionCards title="Disney" videos={disneyVideos} size="large" />
-        <SectionCards title="Travel" videos={travelVideos} size="small" />
-        <SectionCards
-          title="Productivity"
-          videos={productivityVideos}
-          size="medium"
+      <div className={S.main}>
+        <NavBar userName="roigaon@gmail.com" />
+        <Banner
+          title="Clifford the red dog"
+          subTitle="a very cute dog"
+          imgUrl="/static/clifford.webp"
         />
-        <SectionCards title="Popular" videos={popularVideos} size="small" />
+        <div className={S.sectionWrapper}>
+          <SectionCards title="Disney" videos={disneyVideos} size="large" />
+          <SectionCards title="Travel" videos={travelVideos} size="small" />
+          <SectionCards
+            title="Productivity"
+            videos={productivityVideos}
+            size="medium"
+          />
+          <SectionCards title="Popular" videos={popularVideos} size="small" />
+        </div>
       </div>
     </div>
   );
