e8e59b33df4cf8e002ad9d66c673ce449756b973
RoiGaon <roigaon@gmail.com>
2022-06-25 14:34:58 +0300

    adding login page

---
+++ pages/login.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+// Next
+import Head from "next/head";
+
+export default function login() {
+  return (
+    <>
+      <Head>
+        <title>Netflix SignIn</title>
+      </Head>
+      <div>this is the login page</div>
+    </>
+  );
+}
