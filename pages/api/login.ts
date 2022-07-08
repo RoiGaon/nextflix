08e3ca49cfde8c76ae5e14a48e87c06d6b26593c
RoiGaon <roigaon@gmail.com>
2022-07-08 18:04:05 +0300

    adding magicAdmin for server usage

--- pages/api/login.ts
+++ pages/api/login.ts
@@ -1,12 +1,19 @@
+import { magicAdmin } from "@lib/magic-server";
 import type { NextApiRequest, NextApiResponse } from "next";
 
 type Data = {
   name: string;
 };
 
-export default function login(req: NextApiRequest, res: NextApiResponse<Data>) {
+export default async function login(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) {
   if (req.method === "POST") {
     try {
+      const auth = req.headers.authorization;
+      const didToken = auth ? auth.substring(7) : "";
+      const metadata = await magicAdmin.users.getMetadataByToken(didToken);
       res.status(200).json({ name: "John Doe" });
     } catch (error) {
       console.log("Something went wrong logging in " + error);
