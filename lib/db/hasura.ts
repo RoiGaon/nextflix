8901e188494fb5e1372e042b5dec47764b45e657
RoiGaon <roigaon@gmail.com>
2022-07-20 21:55:50 +0300

    building a check query for a new/old user

--- lib/db/hasura.ts
+++ lib/db/hasura.ts
@@ -1,46 +1,42 @@
+export const isNewUser = async (token: string, issuer: string) => {
+  const operationsDoc = `
+  query isNewUser($issuer: String!) {
+    users(where: {issuer: {_eq: $issuer}}) {
+      id
+      email
+      issuer
+    }
+  }
+`;
+  const res = await queryHasuraGQL(
+    operationsDoc,
+    "isNewUser",
+    {
+      issuer,
+    },
+    token
+  );
+  return res?.users?.length === 0;
+};
+
 const queryHasuraGQL = async (
   operationsDoc: string,
   operationName: string,
-  variables: any
+  variables: any,
+  token: string
 ) => {
   const result = await fetch(process.env.NEXT_PUBLIC_HASURA_ADMIN_URL!, {
     method: "POST",
+    headers: {
+      Authorization: `Bearer ${token}`,
+      "Content-type": "application/json",
+    },
     body: JSON.stringify({
       query: operationsDoc,
       operationName: operationName,
       variables: variables,
     }),
-    headers: {
-      "x-hasura-admin-secret": process.env.NEXT_PUBLIC_HASURA_ADMIN_SECRET!,
-    },
   });
 
   return await result.json();
 };
-
-const operationsDoc = `
-    query MyQuery {
-      users {
-        email
-        id
-        issuer
-        publicAddress
-      }
-    }
-  `;
-
-function fetchMyQuery() {
-  return queryHasuraGQL(operationsDoc, "MyQuery", {});
-}
-
-export async function startFetchMyQuery() {
-  const { errors, data } = await fetchMyQuery();
-
-  if (errors) {
-    // handle those errors like a pro
-    console.error(errors);
-  }
-
-  // do something great with this precious data
-  console.log(data);
-}
