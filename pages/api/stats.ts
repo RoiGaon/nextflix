020831ffacd378b471063f39e8d3640ca43f0a94
RoiGaon <roigaon@gmail.com>
2022-09-10 16:22:27 +0300

    adding findVideoId in the stats api

--- pages/api/stats.ts
+++ pages/api/stats.ts
@@ -1,5 +1,6 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import { jwt } from "jsonwebtoken";
+import { findVideoIdByUser } from "@lib/db/hasura";
 
 export default async function stats(
   req: NextApiRequest,
@@ -10,9 +11,16 @@ export default async function stats(
       const token = req.cookies.token;
       // check for a token
       if (!token) return res.status(403).json({ message: "Forbidden client" });
+      const videoId = String(req.query.videoId);
       // verify token
       const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
-      res.status(200).json({ done: true, decodedToken });
+      const findVideoId = await findVideoIdByUser(
+        token,
+        decodedToken.issuer,
+        videoId
+      );
+
+      res.status(200).json({ done: true, decodedToken, findVideoId });
     } catch (error) {
       console.log("Error occured /stats ", error);
       res.status(500).json({ done: false, error });
