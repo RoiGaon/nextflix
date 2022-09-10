5c12d44800205a11d8a54dadd6ee282418eb9d5f
RoiGaon <roigaon@gmail.com>
2022-09-10 15:44:05 +0300

    adding token check + verification logic

--- pages/api/stats.ts
+++ pages/api/stats.ts
@@ -1,10 +1,21 @@
 import { NextApiRequest, NextApiResponse } from "next";
+import { jwt } from "jsonwebtoken";
 
 export default async function stats(
   req: NextApiRequest,
   res: NextApiResponse<any>
 ) {
   if (req.method === "POST") {
-    res.status(200).json({ done: true });
+    try {
+      const token = req.cookies.token;
+      // check for a token
+      if (!token) return res.status(403).json({ message: "Forbidden client" });
+      // verify token
+      const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
+      res.status(200).json({ done: true, decodedToken });
+    } catch (error) {
+      console.log("Error occured /stats ", error);
+      res.status(500).json({ done: false, error });
+    }
   }
 }
