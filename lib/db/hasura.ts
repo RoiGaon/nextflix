7a8671b19fcc0993ad63e473a88f703ad586d7f8
RoiGaon <roigaon@gmail.com>
2022-09-10 13:13:41 +0300

    sorting the page + adding createNewUser func

--- lib/db/hasura.ts
+++ lib/db/hasura.ts
@@ -1,3 +1,25 @@
+const queryHasuraGQL = async (
+  operationsDoc: string,
+  operationName: string,
+  variables: any,
+  token: string
+) => {
+  const result = await fetch(process.env.NEXT_PUBLIC_HASURA_ADMIN_URL!, {
+    method: "POST",
+    headers: {
+      Authorization: `Bearer ${token}`,
+      "Content-type": "application/json",
+    },
+    body: JSON.stringify({
+      query: operationsDoc,
+      operationName: operationName,
+      variables: variables,
+    }),
+  });
+
+  return await result.json();
+};
+
 export const isNewUser = async (token: string, issuer: string) => {
   const operationsDoc = `
   query isNewUser($issuer: String!) {
@@ -19,24 +41,29 @@ export const isNewUser = async (token: string, issuer: string) => {
   return res?.users?.length === 0;
 };
 
-const queryHasuraGQL = async (
-  operationsDoc: string,
-  operationName: string,
-  variables: any,
-  token: string
-) => {
-  const result = await fetch(process.env.NEXT_PUBLIC_HASURA_ADMIN_URL!, {
-    method: "POST",
-    headers: {
-      Authorization: `Bearer ${token}`,
-      "Content-type": "application/json",
+export const createNewUser = async (token: string, metadata: any) => {
+  const { id, email, issuer, publicAddress } = metadata;
+  const operationsDoc = `
+  mutation createNewUser($email: String!, $issuer: String!, $publicAddress: String!) {
+    insert_users(objects: {email: $email, issuer: $issuer, publicAddress: $publicAddress}){
+      returning {
+        email
+        id
+        issuer
+      }
+    }
+  }
+`;
+  const res = await queryHasuraGQL(
+    operationsDoc,
+    "createNewUser",
+    {
+      id,
+      email,
+      issuer,
+      publicAddress,
     },
-    body: JSON.stringify({
-      query: operationsDoc,
-      operationName: operationName,
-      variables: variables,
-    }),
-  });
-
-  return await result.json();
+    token
+  );
+  return res?.users?.length === 0;
 };
