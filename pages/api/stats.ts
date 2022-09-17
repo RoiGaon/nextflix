1e24d027172ced609dc34e2a925c6a7aa1cc9c10
RoiGaon <roigaon@gmail.com>
2022-09-17 10:47:29 +0300

    refactor: make it better

--- pages/api/stats.ts
+++ pages/api/stats.ts
@@ -7,27 +7,25 @@ export default async function stats(
   req: NextApiRequest,
   res: NextApiResponse<any>
 ) {
-  if (req.method === "POST") {
-    try {
-      const token = req.cookies.token;
-      // check for a token
-      if (!token)
-        return res
-          .status(403)
-          .json({ done: false, message: "Forbidden client" });
-      const { videoId, favourited, watched = true } = req.body;
-      if (!videoId)
-        return res
-          .status(400)
-          .json({ done: false, message: "missing videoId" });
-      // verify token
-      const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
-      const findVideo = await findVideoIdByUser(
-        token,
-        decodedToken.issuer,
-        videoId
-      );
-      const doesStatExist = findVideo?.length > 0;
+  try {
+    const { videoId } = req.body;
+    const token = req.cookies.token;
+    // check for a token
+    if (!token)
+      return res.status(403).json({ done: false, message: "Forbidden client" });
+    if (!videoId)
+      return res.status(400).json({ done: false, message: "missing videoId" });
+    // verify token
+    const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
+    const findVideo = await findVideoIdByUser(
+      token,
+      decodedToken.issuer,
+      videoId
+    );
+    const doesStatExist = findVideo?.length > 0;
+    if (req.method === "POST") {
+      const { favourited, watched = true } = req.body;
+
       if (doesStatExist) {
         // update video
         const updateVideo: VideoStatGraphQLData = {
@@ -49,39 +47,15 @@ export default async function stats(
         const insertRes = await insertStat(token, insertVideo);
         return res.status(200).json({ done: true, insertRes });
       }
-    } catch (error) {
-      console.log("Error occured /stats ", error);
-      return res.status(500).json({ done: false, error });
-    }
-  } else if (req.method === "GET") {
-    try {
-      const token = req.cookies.token;
-      // check for a token
-      if (!token)
-        return res
-          .status(403)
-          .json({ done: false, message: "Forbidden client" });
-      const { videoId } = req.body;
-      if (!videoId)
-        return res
-          .status(400)
-          .json({ done: false, message: "missing videoId" });
-      // verify token
-      const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
-      const findVideo = await findVideoIdByUser(
-        token,
-        decodedToken.issuer,
-        videoId
-      );
-      const doesStatExist = findVideo?.length > 0;
+    } else if (req.method === "GET") {
       if (doesStatExist) {
         return res.status(200).json({ done: true, findVideo });
       } else {
         return res.status(404).json({ done: true, msg: "video was not found" });
       }
-    } catch (error) {
-      console.log("Error occured /stats ", error);
-      return res.status(500).json({ done: false, error });
     }
+  } catch (error) {
+    console.log("Error occured /stats ", error);
+    return res.status(500).json({ done: false, error });
   }
 }
